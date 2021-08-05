9bc80786a6db0a4fba06819c5a2c56ce14dca6ef
Leyzeld F <leyzeld@gmail.com>
2021-08-05 15:02:10 +0300

    Added armor class with helmet torso and leggings

---
+++ TheBestGame/Armor.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheBestGame
+{
+    class Armor
+    {
+        //private int id;
+        //private Bitmap skin;
+        private string name;
+        private int defense;
+
+        public Armor()
+        {
+            name = "test armor";
+            defense = 20;
+        }
+        public Armor(string _name, int _defense)
+        {
+            name = _name;
+            defense = _defense;
+        }
+        public string GetName()
+        {
+            return name;
+        }
+
+        public int GetDefense()
+        {
+            return defense;
+        }
+        public void SetName(string _name)
+        {
+            name = _name;
+        }
+
+        public void SetDefense(int _defense)
+        {
+            defense = _defense;
+        }
+    }
+
+    class Helmet : Armor
+    {
+
+    }
+    class Torso : Armor
+    {
+
+    }
+    class Leggings : Armor
+    {
+
+    }
+}
